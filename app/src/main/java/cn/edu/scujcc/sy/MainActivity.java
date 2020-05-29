d092c2f309d646126c1b3b6f7d89d43cb42d1ab6
Eric <58718764+E-J122@users.noreply.github.com>
2020-05-29 11:03:18 +0800

    完成点击拍摄按钮跳转功能

--- app/src/main/java/cn/edu/scujcc/sy/MainActivity.java
+++ app/src/main/java/cn/edu/scujcc/sy/MainActivity.java
@@ -2,13 +2,20 @@ package cn.edu.scujcc.sy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button shootButton,importButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        shootButton = findViewById(R.id.shoot);
+        shootButton.setOnClickListener(v ->{
+            Intent intent = new Intent(MainActivity.this,ShootActivity.class);
+            startActivity(intent);
+        });
     }
 }
