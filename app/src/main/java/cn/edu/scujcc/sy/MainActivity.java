1f9f322763eca845a3a17980b7fbe87e0395b0fd
Eric <58718764+E-J122@users.noreply.github.com>
2020-06-02 15:08:16 +0800

    实现调用系统原生摄像头功能，可进行摄像。

--- app/src/main/java/cn/edu/scujcc/sy/MainActivity.java
+++ app/src/main/java/cn/edu/scujcc/sy/MainActivity.java
@@ -4,18 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button shootButton,importButton;
+    private Button importButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        shootButton = findViewById(R.id.shoot);
-        shootButton.setOnClickListener(v ->{
-            Intent intent = new Intent(MainActivity.this,ShootActivity.class);
-            startActivity(intent);
-        });
+        importButton = findViewById(R.id.import_video);
+    }
+
+    public void click(View view){
+        Intent intent=new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        startActivity(intent);
     }
 }
